588594af63773016b21797540fe72a80adbf6e45
Sev O <severin.obertuefer@air-up.com>
2024-09-10 13:42:20 +0200

    Update product.ts

--- src/util/product.ts
+++ src/util/product.ts
@@ -14,7 +14,7 @@ export class ProductService {
             "x-airup-app": "product",
         };
         const base = 'https://webshop-gateway-stage.svc.air-up.com';
-        const response = await fetch(`${base}/v3/products/categories/${category}?region=dev1`, { headers, method: 'GET' });
+        const response = await fetch(`${base}/v3/products/categories/${category}?region=dev1`, { headers, method: 'GET', cache: 'no-store' });
 
         if (!response.ok) {
             const responseBody = await response.text();
