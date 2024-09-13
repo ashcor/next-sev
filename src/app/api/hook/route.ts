5bb8df1190e25bc37ee967291439ae0698d4ee65
Sev O <severin.obertuefer@air-up.com>
2024-09-13 13:24:37 +0200

    Create route.ts

---
+++ src/app/api/hook/route.ts
@@ -0,0 +1,9 @@
+
+export async function GET() {
+    await fetch('https://webshop-gahttps://mmneolqbpa.execute-api.eu-central-1.amazonaws.com/hook/log', {
+        method: 'POST',
+        body: JSON.stringify({tag: 'flush', time: Date.now()}),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    return Response.json({ ok: true, revalidated: true, now: Date.now() })
+}
