7a21f99883bfc8f200f9894464e310a9556c0e20
Sev O <severin.obertuefer@air-up.com>
2024-10-03 10:17:53 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,20 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const { hostname } = req.nextUrl;
 
-  console.log('Request to', hostname);
+    console.log('Host', req.nextUrl.host);
+    console.log('Hostname', req.nextUrl.hostname);
+    console.log('Href', req.nextUrl.href);
+    console.log('Origin', req.nextUrl.origin);
+    console.log('Pathname', req.nextUrl.pathname);
+    console.log('Search', req.nextUrl.search);
+    console.log('BasePath', req.nextUrl.basePath);
+    console.log('BuildId', req.nextUrl.buildId);
 
-  // Check the subdomain
-  if (hostname.startsWith('a.')) {
-    return NextResponse.redirect(new URL('/a', req.url));
-  }
-
-  if (hostname.startsWith('b.')) {
-    return NextResponse.redirect(new URL('/b', req.url));
-  }
-
-  // Default behavior if no subdomain matches
-  return NextResponse.next();
+    return NextResponse.next();
 }
 
 // Define the paths where middleware should apply
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
