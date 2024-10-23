8a3921a2c0d1abd9f04182dfb3e599c72d532518
Sev O <severin.obertuefer@air-up.com>
2024-10-23 09:19:29 +0200

    simplify

--- next.config.js
+++ next.config.js
@@ -12,55 +12,18 @@ const nextConfig = {
         },
     },
     async redirects() {
-
-        const legacyRedirects = (pfs, pfd) => [
-            {
-                source: `${pfs}/legacy/kola`,
-                destination: `/shop/${pfd}/flavors/kola`,
-                permanent: true,
-            },
-        ];
-
-        const legacyCatchAllRedirects = (pfs, pfd) => [
-            {
-                source: `${pfs}/legacy/:slug*`,
-                destination: `${pfd}/`,
-                permanent: true,
-            },
-        ];
-
-        const redirects = [
-            ...legacyRedirects("", ""),
-            ...legacyRedirects("/:country([a-z]{2})", "/:country"),
-            ...legacyCatchAllRedirects("", ""),
-            ...legacyCatchAllRedirects("/:country([a-z]{2})", "/:country"),
-        ];
-
-        // same as redirects but just hardcode the values
-        const redirects2 = [
-            // {
-            //     source: "/legacy/kola",
-            //     destination: "/shop/flavors/kola",
-            //     permanent: true,
-            // },
+        return [
             {
                 source: "/:country([a-z]{2})/legacy/kola",
                 destination: "/shop/:country/flavors/kola",
                 permanent: true,
             },
-            // {
-            //     source: "/legacy/:slug*",
-            //     destination: "/",
-            //     permanent: true,
-            // },
             {
                 source: "/:country([a-z]{2})/legacy/:slug*",
                 destination: "/:country",
                 permanent: true,
             },
         ];
-
-        return redirects2;
     },
 }
 
